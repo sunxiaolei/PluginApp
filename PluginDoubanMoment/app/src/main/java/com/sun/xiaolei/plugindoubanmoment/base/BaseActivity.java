9783b72be82c3f918e1efeef84471f600bb64721
sunxiaolei <xiaoleisun92@gmail.com>
2017-08-10 17:51:49 +0800

    夜间模式

--- PluginDoubanMoment/app/src/main/java/com/sun/xiaolei/plugindoubanmoment/base/BaseActivity.java
+++ PluginDoubanMoment/app/src/main/java/com/sun/xiaolei/plugindoubanmoment/base/BaseActivity.java
@@ -3,8 +3,13 @@ package com.sun.xiaolei.plugindoubanmoment.base;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import sunxl8.library.swipeback.SwipeBackActivityHelper;
+import sunxl8.library.swipeback.SwipeBackLayout;
+import sunxl8.library.swipeback.Utils;
 
 
 /**
@@ -13,17 +18,42 @@ import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 public abstract class BaseActivity extends RxAppCompatActivity {
 
+    protected abstract int setContentViewId();
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected abstract void init();
+
+    private SwipeBackActivityHelper mHelper;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mHelper = new SwipeBackActivityHelper(this);
+        this.mHelper.onActivityCreate();
         setContentView(setContentViewId());
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         init();
     }
 
-    protected abstract int setContentViewId();
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.mHelper.onPostCreate();
+    }
 
-    protected abstract void init();
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        return v == null && this.mHelper != null ? this.mHelper.findViewById(id) : v;
+    }
+
+    public SwipeBackLayout getSwipeBackLayout() {
+        return this.mHelper.getSwipeBackLayout();
+    }
+
+    public void setSwipeBackEnable(boolean enable) {
+        this.getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        this.getSwipeBackLayout().scrollToFinishActivity();
+    }
 
 }
