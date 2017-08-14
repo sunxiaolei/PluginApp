da04efa16bf748bfa38850ca704c5298e7061d81
sunxl8 <sunxl8@centaline.com.cn>
2017-08-14 16:41:32 +0800

    ui

--- PluginTest1/app/src/main/java/com/sun/xiaolei/plugintest1/ui/MainActivity.java
+++ PluginTest1/app/src/main/java/com/sun/xiaolei/plugintest1/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sun.xiaolei.plugintest1.R;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.tv_test);
         tv.setText(str == null ? "Plugin 1 update" : str);
 
+        ImageView iv = (ImageView) findViewById(R.id.iv_test);
 
         rvTest = (RecyclerView) findViewById(R.id.rv_test);
         rvTest.setLayoutManager(new LinearLayoutManager(this));
@@ -38,5 +40,6 @@ public class MainActivity extends AppCompatActivity {
         }
         TestAdapter mAdapter = new TestAdapter(this, test);
         rvTest.setAdapter(mAdapter);
+
     }
 }
